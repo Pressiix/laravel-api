f6d8fd6c060e3bf4ffbe9f926c207a16c27a9447
watcharaphonp <watcharaphon@treg.co.th>
2024-08-05 17:49:20 +0700

    create user api and migrate post table

---
+++ apps/database/migrations/2024_08_05_104701_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->string('title'); // Title of the post
+            $table->text('content'); // Content of the post
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade'); // Foreign key to users table
+            $table->timestamps(); // Created at and updated at timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
