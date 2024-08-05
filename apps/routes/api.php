d0b93184c26ef107720ba3eccf6bb1944ef4a05d
watcharaphonp <watcharaphon@treg.co.th>
2024-08-05 18:28:34 +0700

    implement route group

--- apps/routes/api.php
+++ apps/routes/api.php
@@ -4,16 +4,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 
-// users
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+// Users
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::post('/', [UserController::class, 'store'])->name('users.store');
+    Route::put('/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
-// posts
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::put('/posts/{id}', [PostController::class, 'update']);
-Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+// Posts
+Route::prefix('posts')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::post('/', [PostController::class, 'store'])->name('posts.store');
+    Route::put('/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+});
